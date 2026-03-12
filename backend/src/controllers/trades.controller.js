e9330f2f024ba1775f8bdd11975c872c08b71fdb
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2026-03-12 17:47:11 -0500

    Add getTrade controller to fetch a specific user trade

--- backend/src/controllers/trades.controller.js
+++ backend/src/controllers/trades.controller.js
@@ -14,4 +14,25 @@ async function getAllTrades(req, res) {
   }
 }
 
-module.exports = { getAllTrades };
+async function getTrade(req, res) {
+  try {
+    const tradeId = req.params.id;
+    const tradeEntry = await Trade.findById(tradeId);
+
+    if (!tradeEntry) {
+      return res.status(404).json({ message: "Trade entry not found." });
+    }
+
+    if (tradeEntry.userId !== req.userId) {
+      return res.status(401).json({ message: "Trade entry not found." });
+    }
+
+    return res.status(200).json({
+      tradeEntry,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = { getAllTrades, getTrade };
