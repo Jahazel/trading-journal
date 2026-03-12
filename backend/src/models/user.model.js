6627ccd0b1fa209604b086d39be50f4d4c731474
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2026-03-12 12:57:18 -0500

    Add validation constraints to user schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    uinique: true,
+    lowercase: true,
+    trim: true,
+    minLength: 1,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: { type: String, required: true, minLength: 6, maxLength: 50 },
   createdAt: { type: Date, default: Date.now },
 });
 
